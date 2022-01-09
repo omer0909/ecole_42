18a099f6abe1c12c6585b14c204f606f05e0d11b
Omer faruk Oz <ooz@k1m08s06.42istanbul.com.tr>
2022-01-09 15:00:58 +0300

    a

---
+++ libft/ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ooz <ozomerfaruk009@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 12:14:27 by ooz               #+#    #+#             */
+/*   Updated: 2022/01/03 12:29:35 by ooz              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	i;
+	void	*r;
+
+	i = nmemb * size;
+	if (i > 0X7FFFFFFF)
+		return (NULL);
+	r = malloc(i);
+	if (!r)
+		return (NULL);
+	ft_bzero(r, i);
+	return (r);
+}
---
+++ libft/ft_lstmap.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ooz <ozomerfaruk009@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 12:15:56 by ooz               #+#    #+#             */
+/*   Updated: 2022/01/03 12:15:56 by ooz              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *))
+{
+	t_list	*new;
+
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	new = ft_lstnew(f(lst->content));
+	if (new == 0)
+	{
+		del(&(lst->content));
+		new = ft_lstmap(lst->next, f, del);
+	}
+	else
+		new->next = ft_lstmap(lst->next, f, del);
+	return (new);
+}
